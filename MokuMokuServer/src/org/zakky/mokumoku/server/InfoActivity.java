3e23ffedd8017e3b56e194a7c981e666c718a916
YAMAZAKI Makoto <makoto1975@gmail.com>
2011-05-04 10:29:19 +0900

    サーバ機能をサービス化しました。

--- MokuMokuServer/src/org/zakky/mokumoku/server/InfoActivity.java
+++ MokuMokuServer/src/org/zakky/mokumoku/server/InfoActivity.java
@@ -1,25 +1,11 @@
 
 package org.zakky.mokumoku.server;
 
-import java.io.IOException;
-import java.net.Inet6Address;
-import java.net.InetSocketAddress;
-import java.net.SocketAddress;
-import java.net.UnknownHostException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
-import java.nio.channels.spi.SelectorProvider;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ListIterator;
-
 import android.app.Activity;
 import android.app.ProgressDialog;
-import android.os.AsyncTask;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class InfoActivity extends Activity {
@@ -30,168 +16,23 @@ public class InfoActivity extends Activity {
         setContentView(R.layout.main);
     }
 
-    private AsyncTask<Void, String, Void> mServer;
-
     @Override
     protected void onStart() {
         super.onStart();
 
-        if (mServer != null) {
-            mServer.cancel(true);
-        }
-        mServer = new AsyncTask<Void, String, Void>() {
-            private final ProgressDialog mProgress = new ProgressDialog(InfoActivity.this);
+        final Intent serviceIntent = new Intent(this, StatusServerService.class);
+        startService(serviceIntent);
 
-            private final List<ClientStatus> mClients = new ArrayList<ClientStatus>();
-
-            {
-                for (int i = 0; i < 8; i++) {
-                    mClients.add(new ClientStatus());
-                }
-            }
+        final ProgressDialog progress = ProgressDialog.show(this, "Status Server", "");
+        progress.setCancelable(true);
+        progress.setOnCancelListener(new OnCancelListener() {
 
             @Override
-            protected void onPreExecute() {
-                super.onPreExecute();
-
-                mProgress.setTitle("Running MokuMoku Server");
-                mProgress.show();
+            public void onCancel(DialogInterface dialog) {
+                final Intent serviceIntent = new Intent(InfoActivity.this,
+                        StatusServerService.class);
+                stopService(serviceIntent);
             }
-
-            @Override
-            protected Void doInBackground(Void... params) {
-                try {
-                    final Selector selector = SelectorProvider.provider().openSelector();
-
-                    final ServerSocketChannel serverSocket = ServerSocketChannel.open();
-                    serverSocket.configureBlocking(false);
-
-                    final SocketAddress localSockAddr = new InetSocketAddress(
-                            Inet6Address.getByName("::"), 2525);
-                    serverSocket.socket().bind(localSockAddr, 10);
-
-                    serverSocket.register(selector, SelectionKey.OP_ACCEPT);
-
-                    publishProgress("waiting client");
-                    while (true) {
-                        final ByteBuffer buffer = ByteBuffer.allocate(4096);
-                        selector.select();
-                        for (SelectionKey k : selector.selectedKeys()) {
-                            if (k.isAcceptable()) {
-                                final ServerSocketChannel s = (ServerSocketChannel) k.channel();
-                                final SocketChannel clientSocket = s.accept();
-                                clientSocket.configureBlocking(false);
-
-                                publishProgress("waiting client");
-
-                                clientSocket.register(selector, SelectionKey.OP_READ);
-                            }
-                            if (k.isReadable()) {
-                                final SocketChannel s = (SocketChannel) k.channel();
-                                buffer.clear();
-                                s.read(buffer);
-                                buffer.flip();
-                                buffer.order(ByteOrder.BIG_ENDIAN);
-                                final int clientId = buffer.getInt();
-                                final int length = buffer.getInt();
-                                if (0 < length) {
-                                    updateClientStatus(clientId, length, buffer);
-                                }
-                                sendClientStatus(s);
-                            }
-                        }
-                    }
-                } catch (UnknownHostException e) {
-                    throw new RuntimeException(e);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
-            /**
-             * 
-             * @param clientId
-             * @param length
-             * @param buffer
-             * @return 更新できた場合は {@code true}、上限に達していて破棄された場合は
-             * {@code false}。
-             */
-            private boolean updateClientStatus(int clientId, int length, ByteBuffer buffer) {
-                if (length != buffer.remaining()) {
-                    // TODO 丸ごと読めなかった場合に対処する
-                    throw new RuntimeException("invalid length: " + length + "(header) "
-                            + buffer.remaining() + "(actual)");
-                }
-
-                final ClientStatus clientStatus = new ClientStatus(clientId, buffer.array(),
-                        buffer.arrayOffset() + buffer.position(), buffer.remaining());
-
-                int openStatusIndex = -1;
-                int prevStatusIndex = -1;
-                int index = 0;
-                final ListIterator<ClientStatus> iterator = mClients.listIterator();
-                while (iterator.hasNext()) {
-                    final ClientStatus status = iterator.next();
-                    if (status.getId() == clientId) {
-                        // found
-                        prevStatusIndex = index;
-                        break;
-                    }
-                    if (openStatusIndex == -1 && status.getId() == ClientStatus.INVALID_ID) {
-                        openStatusIndex = index;
-                    }
-                    index++;
-                }
-                if (prevStatusIndex == -1) {
-                    // new client
-                    if (openStatusIndex == -1) {
-                        // クライアント数上限
-                        return false;
-                    }
-                    // 新規クライアントなので、openなところを使用する。
-                    prevStatusIndex = openStatusIndex;
-                }
-
-                mClients.set(prevStatusIndex, clientStatus);
-                return true;
-            }
-
-            private void sendClientStatus(SocketChannel socket) {
-                int totalLength = 4; // length(4)
-                for (ClientStatus client : mClients) {
-                    totalLength += 8; // id(4) and length(4)
-                    totalLength += client.getPayload().length;
-                }
-
-                final ByteBuffer buffer = ByteBuffer.allocate(totalLength);
-                buffer.order(ByteOrder.BIG_ENDIAN);
-                buffer.putInt(totalLength - 4);
-                for (ClientStatus client : mClients) {
-                    buffer.putInt(client.getId());
-                    buffer.putInt(client.getPayload().length);
-                    buffer.put(client.getPayload());
-                }
-            }
-
-            @Override
-            protected void onProgressUpdate(String... values) {
-                super.onProgressUpdate(values);
-                mProgress.setMessage(values[0]);
-            }
-
-            @Override
-            protected void onCancelled() {
-                super.onCancelled();
-                mProgress.dismiss();
-            }
-
-            @Override
-            protected void onPostExecute(Void result) {
-                super.onPostExecute(result);
-                mProgress.dismiss();
-            }
-
-        };
-        mServer.execute();
+        });
     }
 }
---
+++ MokuMokuServer/src/org/zakky/mokumoku/server/StatusServerService.java
@@ -0,0 +1,259 @@
+
+package org.zakky.mokumoku.server;
+
+import java.io.IOException;
+import java.net.Inet6Address;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.ClosedChannelException;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.channels.spi.SelectorProvider;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class StatusServerService extends Service {
+
+    public static final String TAG = StatusServerService.class.getSimpleName();
+
+    private static final String HOST = "::";
+
+    private static final int PORT = 2525;
+
+    private static final int BACKLOG = 10;
+
+    //private int mStartId = -1;
+
+    private ServerThread mServerThread;
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(TAG, "onStartCommand()");
+
+        super.onStartCommand(intent, flags, startId);
+
+        // mStartId = startId;
+
+        try {
+            mServerThread = new ServerThread(HOST, PORT, BACKLOG);
+            mServerThread.start();
+
+        } catch (IOException e) {
+            Log.e(TAG, "failed to bind server socket to [" + HOST + "]:" + PORT);
+        }
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy()");
+
+        final ServerThread serverThread = mServerThread;
+        if (serverThread != null) {
+            serverThread.quit();
+        }
+    }
+
+}
+
+final class ServerThread extends Thread {
+
+    private volatile boolean mQuit;
+
+    private final Selector mSelector;
+
+    private final List<ClientStatus> mClients;
+
+    private final ByteBuffer mReceiveBuffer;
+
+    public ServerThread(String host, int port, int backlog) throws IOException {
+        super();
+        mQuit = false;
+        mClients = createInitialStatusList();
+        mSelector = createSocketSelector(host, port, backlog);
+        mReceiveBuffer = ByteBuffer.allocate(4096);
+    }
+
+    private static List<ClientStatus> createInitialStatusList() {
+        final List<ClientStatus> statusList = new ArrayList<ClientStatus>();
+        for (int i = 0; i < 8; i++) {
+            statusList.add(new ClientStatus());
+        }
+        return statusList;
+    }
+
+    private static Selector createSocketSelector(String host, int port, int backlog)
+            throws IOException {
+        final Selector selector = SelectorProvider.provider().openSelector();
+
+        final ServerSocketChannel serverSocket = ServerSocketChannel.open();
+        serverSocket.configureBlocking(false);
+        serverSocket.socket().setReceiveBufferSize(8192);
+        serverSocket.socket().setReuseAddress(true);
+
+        final SocketAddress localSockAddr = new InetSocketAddress(Inet6Address.getByName(host),
+                port);
+        serverSocket.socket().bind(localSockAddr, backlog);
+        serverSocket.register(selector, SelectionKey.OP_ACCEPT);
+
+        return selector;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        // publishProgress("waiting client");
+        try {
+            while (!mQuit) {
+                mSelector.select();
+                for (SelectionKey key : mSelector.selectedKeys()) {
+                    if (key.isAcceptable()) {
+                        handleAcceptable(key);
+                    }
+                    if (key.isWritable()) {
+                        handleWritable(key);
+                    }
+                    if (key.isReadable()) {
+                        handleReadable(key);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            Log.e(StatusServerService.TAG, "failed to select", e);
+        } finally {
+            try {
+                mSelector.close();
+            } catch (IOException e) {
+                Log.e(StatusServerService.TAG, "failed to close Selector", e);
+            }
+            Log.i(StatusServerService.TAG, "server thread finised.");
+        }
+    }
+
+    public void quit() {
+        mQuit = true;
+        interrupt();
+    }
+
+    private void handleAcceptable(SelectionKey key) {
+        final ServerSocketChannel s = (ServerSocketChannel) key.channel();
+        try {
+            final SocketChannel clientSocket = s.accept();
+            clientSocket.configureBlocking(false);
+
+            clientSocket.register(mSelector, SelectionKey.OP_READ);
+        } catch (ClosedChannelException e) {
+            Log.i(StatusServerService.TAG, "server socket closed.", e);
+        } catch (IOException e) {
+            Log.i(StatusServerService.TAG, "failed to accept connection.", e);
+        }
+    }
+
+    private void handleWritable(SelectionKey key) {
+    }
+
+    private void handleReadable(SelectionKey key) {
+        final SocketChannel s = (SocketChannel) key.channel();
+        mReceiveBuffer.clear();
+        try {
+            s.read(mReceiveBuffer); // TODO 一回で読めるとは限らない
+            mReceiveBuffer.flip();
+            mReceiveBuffer.order(ByteOrder.BIG_ENDIAN);
+            final int clientId = mReceiveBuffer.getInt();
+            final int length = mReceiveBuffer.getInt();
+            if (0 < length) {
+                updateClientStatus(clientId, length, mReceiveBuffer);
+            }
+            sendClientStatus(s);
+        } catch (IOException e) {
+            Log.i(StatusServerService.TAG, "failed to read data from client.", e);
+        }
+    }
+
+    /**
+     * 
+     * @param clientId
+     * @param length
+     * @param buffer
+     * @return 更新できた場合は {@code true}、上限に達していて破棄された場合は
+     * {@code false}。
+     */
+    private boolean updateClientStatus(int clientId, int length, ByteBuffer buffer) {
+        if (length != buffer.remaining()) {
+            // TODO 丸ごと読めなかった場合に対処する
+            throw new RuntimeException("invalid length: " + length + "(header) "
+                    + buffer.remaining() + "(actual)");
+        }
+
+        final ClientStatus clientStatus = new ClientStatus(clientId, buffer.array(),
+                buffer.arrayOffset() + buffer.position(), buffer.remaining());
+
+        int openStatusIndex = -1;
+        int prevStatusIndex = -1;
+        int index = 0;
+        final ListIterator<ClientStatus> iterator = mClients.listIterator();
+        while (iterator.hasNext()) {
+            final ClientStatus status = iterator.next();
+            if (status.getId() == clientId) {
+                // found
+                prevStatusIndex = index;
+                break;
+            }
+            if (openStatusIndex == -1 && status.getId() == ClientStatus.INVALID_ID) {
+                openStatusIndex = index;
+            }
+            index++;
+        }
+        if (prevStatusIndex == -1) {
+            // new client
+            if (openStatusIndex == -1) {
+                // クライアント数上限
+                return false;
+            }
+            // 新規クライアントなので、openなところを使用する。
+            prevStatusIndex = openStatusIndex;
+        }
+
+        mClients.set(prevStatusIndex, clientStatus);
+        return true;
+    }
+
+    private void sendClientStatus(SocketChannel socket) {
+        int totalLength = 4; // length(4)
+        for (ClientStatus client : mClients) {
+            totalLength += 8; // id(4) and length(4)
+            totalLength += client.getPayload().length;
+        }
+
+        final ByteBuffer buffer = ByteBuffer.allocate(totalLength);
+        buffer.order(ByteOrder.BIG_ENDIAN);
+        buffer.putInt(totalLength - 4);
+        for (ClientStatus client : mClients) {
+            buffer.putInt(client.getId());
+            buffer.putInt(client.getPayload().length);
+            buffer.put(client.getPayload());
+        }
+        buffer.flip();
+        try {
+            socket.register(mSelector, SelectionKey.OP_READ | SelectionKey.OP_WRITE, buffer);
+        } catch (ClosedChannelException e) {
+            Log.i(StatusServerService.TAG, "socket closed.", e);
+        }
+    }
+}
